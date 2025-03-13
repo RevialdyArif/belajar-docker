f8e542fd31eba151d1f96598d8f48d8c33f5a159
revi aldy <revialdy.arifs@gmail.com>
2025-03-13 15:39:41 +0700

    aaaaa

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='text-6-xl font-bold text-center'>Testing Docker</div>
+    <div className='text-6-xl font-bold text-center'>
+      Testing Update Docker
+
+      <p>Apakah terupdate?</p>
+    </div>
   )
 }
